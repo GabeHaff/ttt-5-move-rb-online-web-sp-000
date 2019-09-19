b92c204c4c96f3e8de7d2f5314baae5ceb4af301
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-19 13:34:01 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
   input.to_i-1
   end 
 
-def move(display_board, position, character=X)
+def move(display_board, position, character="X")
   display_board[index]=character
 end 
